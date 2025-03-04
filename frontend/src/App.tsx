bf62c930158c1903f32219b069f93e3a0c52f269
fffirer <0sdf0@sina.cn>
2025-03-04 23:22:35 +0800

    前端初始化

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,45 @@
-import type { Component } from 'solid-js';
+import { Suspense, type Component } from 'solid-js';
+import { A, useLocation } from '@solidjs/router';
 
-const App: Component = () => {
+const App: Component = (props: { children: Element }) => {
+  const location = useLocation();
+
   return (
-    <p class="text-4xl text-green-700 text-center py-20">Hello tailwind!</p>
+    <>
+      <nav class="bg-gray-200 text-gray-900 px-4">
+        <ul class="flex items-center">
+          <li class="py-2 px-4">
+            <A href="/" class="no-underline hover:underline">
+              Home
+            </A>
+          </li>
+          <li class="py-2 px-4">
+            <A href="/about" class="no-underline hover:underline">
+              About
+            </A>
+          </li>
+          <li class="py-2 px-4">
+            <A href="/error" class="no-underline hover:underline">
+              Error
+            </A>
+          </li>
+
+          <li class="text-sm flex items-center space-x-1 ml-auto">
+            <span>URL:</span>
+            <input
+              class="w-75px p-1 bg-white text-sm rounded-lg"
+              type="text"
+              readOnly
+              value={location.pathname}
+            />
+          </li>
+        </ul>
+      </nav>
+
+      <main>
+        <Suspense>{props.children}</Suspense>
+      </main>
+    </>
   );
 };
 
