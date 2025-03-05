7506c62d5929b6f39c17213d6294f6479d975a15
fffirer <0sdf0@sina.cn>
2025-03-05 22:57:25 +0800

    重构后端

--- frontend/src/utils/DisplayError.tsx
+++
@@ -1,9 +0,0 @@
-import { ApiException, IdentityError } from "~/services/client";
-
-export const DisplayIdentityError = (e: IdentityError[] | unknown) => {
-  return Array.isArray(e)
-    ? e.map((item) => item.description).join("\r\n")
-    : ApiException.isApiException(e) 
-        ? e.message
-        : "未知错误";
-};
--- frontend/src/utils/swal2.tsx
+++
@@ -1,4 +0,0 @@
-import Swal from "sweetalert2";
-import { withSolidContent } from "sweetalert2-solid-content";
-
-export const solidSwal = withSolidContent(Swal);
