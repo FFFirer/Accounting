d5604b83deedd2c5485286c5caee06770132ba45
fffirer <0sdf0@sina.cn>
2025-03-07 01:05:29 +0800

    feat: theme-control

---
+++ Accounting.Web/Components/Shared/ClassNameHelper.cs
@@ -0,0 +1,10 @@
+﻿namespace Accounting.Web.Components.Shared
+{
+    public static class ClassNameHelper
+    {
+        public static string Merge(params IEnumerable<string> classNames)
+        {
+            return string.Join(" ", classNames);
+        }
+    }
+}
