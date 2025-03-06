e5573f791331560cfec29b8740441393d2fa7573
fffirer <0sdf0@sina.cn>
2025-03-06 22:46:12 +0800

    feat

---
+++ Accounting.Stores/UserRefreshTokenStore.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Accounting
+{
+    public class UserRefreshTokenStore : IUserRefreshTokenStore
+    {
+        protected virtual AccountingDbContext Context { get; set; }
+
+        public UserRefreshTokenStore(AccountingDbContext context)
+        {
+            Context = context;
+        }
+
+        public IQueryable<UserRefreshToken> UserRefreshTokens => Context.UserRefreshTokens;
+    }
+}
