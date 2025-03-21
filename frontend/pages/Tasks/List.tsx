aa96493888e5e0d4a1e6e18eb61d622c251a36f9
fffirer <0sdf0@sina.cn>
2025-03-22 02:54:08 +0800

    使用Table

--- frontend/pages/Tasks/List.tsx
+++ frontend/pages/Tasks/List.tsx
@@ -1,7 +1,15 @@
+import { JobClient } from "@frontend/services/client";
+import { useClient } from "@frontend/utils/useHelper";
 import { Search } from "lucide-solid";
 import { Form, FormControl, FormField } from "solid-form-context";
+import { IndexColumn, Table, TableColumn, TableRow } from "solid-table-context";
+
+const client = useClient(JobClient);
 
 export default () => {
+  const query = (page: number, size: number) =>
+    client.getApiJobQuery(page, size);
+
   return (
     <div class="size-full flex flex-1 flex-col">
       <div class="flex flex-row mb-2 gap-2">
@@ -9,6 +17,7 @@ export default () => {
           <FormField name={"asd"}>
             <FormControl
               control={"input"}
+              controlProps={{ type: "text", class: "input", placeholder: 'Full Name...' }}
               controlValuePropName="value"
               onControlValueChanged={{
                 eventName: "onchange",
@@ -22,7 +31,15 @@ export default () => {
           查询
         </button>
       </div>
-      <div class="grow overflow-x-auto rounded-box border border-base-content/5 bg-base-100"></div>
+      <div class="grow overflow-x-auto rounded-box border border-base-content/5 bg-base-100">
+        <Table class="table table-zebra table-pin-rows">
+          <TableRow class="hover:bg-base-300">
+            <IndexColumn cellClass="font-bold" />
+            <TableColumn name={"namespace"} header="Namespace" />
+            <TableColumn name={"fullName"} header="Full Name" />
+          </TableRow>
+        </Table>
+      </div>
     </div>
   );
 };
