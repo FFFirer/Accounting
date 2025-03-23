5f3c9c039ac22a0439a13ac9c8513da089a5ba42
fffirer <0sdf0@sina.cn>
2025-03-24 01:44:52 +0800

    feat: 设置导航自动Focus到页面主体

--- Accounting.Web.Client/Layout/Navbar.razor.cs
+++ Accounting.Web.Client/Layout/Navbar.razor.cs
@@ -5,38 +5,8 @@ using Microsoft.JSInterop;
 
 namespace Accounting.Web.Client.Layout;
 
-public partial class Navbar : ComponentBase, IDisposable
+public partial class Navbar : ComponentBase
 {
     [Inject]
     private IWebAssemblyHostEnvironment Env { get; set; } = default!;
-
-    [Inject]
-    private NavigationManager NavigationManager { get; set; } = default!;
-
-    [Inject]
-    private ILogger<Navbar> logger { get; set; } = default!;
-
-    [Inject]
-    private IJSRuntime JS {get;set;} = default!;
-
-    public void Dispose()
-    {
-        this.NavigationManager.LocationChanged -= LocationChangedHandler;
-    }
-
-    protected override Task OnInitializedAsync()
-    {
-        if (this.NavigationManager is not null)
-        {
-            NavigationManager.LocationChanged += LocationChangedHandler;
-        }
-
-        return Task.CompletedTask;
-    }
-
-    private void LocationChangedHandler(object? sender, LocationChangedEventArgs e)
-    {
-        this.JS.InvokeVoidAsync("blazorInterop.autoBlur");
-        // InvokeAsync(StateHasChanged);
-    }
 }
